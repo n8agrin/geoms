feaa8792b63e19891ca2f25d1a0e220f2fb31166
Nathan Agrin <n8agrin@gmail.com>
2012-07-26 11:30:56 -0700

    initial commit

---
+++ serve.js
@@ -0,0 +1,16 @@
+
+// Simple script to serve the demos and tests
+
+var static = require('node-static'),
+    http = require('http'),
+    util = require('util'),
+    port = 8980;
+
+var fileServer = new static.Server('./');
+
+http.createServer(function (request, response) {
+    request.addListener('end', function () {
+        console.log('Requesting url', request.url)
+        fileServer.serve(request, response);
+    });
+}).listen(port);
